b437f771ee50aec4099bfaa6824f99b1cbd8cadc
almdashif <ashifalmohammed@gmail.com>
2024-07-02 23:29:25 +0530

    wedding collection completed

--- src/Modules/WeddingCollection/WeddingCollection.jsx
+++ src/Modules/WeddingCollection/WeddingCollection.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-
+import './weddingCollection.css'
 const WeddingCollection = () => {
   return (
     <section id='WeddingCollection'>
       <div className="weddingColContainer">
         <div className="WedImgContainer">
-          <img src="https://imgs.search.brave.com/0tIT40kyRMi-7-lkql_qkcFXNsm_52T-9oEcAazaJmI/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9zMy11/cy13ZXN0LTIuYW1h/em9uYXdzLmNvbS9z/aGFhZGl3aXNobmV3/YnVja2V0LzNjNTQ5/NTcyLThjZTQtMzgy/My0yZWM5LTEyMTlj/NjVmNjM4Mi5qcGc" alt="Wedding Image" />
+          <img src="https://imgs.search.brave.com/vpdvAvCO6PNHAYtKCnxXX7JaC1dneA6lKXC9xK9N0mc/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2VmLzEx/LzFmL2VmMTExZjNi/NWY0MGFiMDJkZGM0/NjVmOTEwMGJhM2I5/LmpwZw" alt="Wedding Image" />
+        </div>
+        <div className="wedTitles">
+          <h3>Wear to Wedding</h3>
+          <p>A symphony of exquistic design tailored for your unforgettable moments</p>
+          <a href="">See Details</a>
         </div>
       </div>
     </section>
